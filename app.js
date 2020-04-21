7a8319de5d20ade14315291bea01021fd7bd31a6
Thomas Nelson <thnlsn@gmail.com>
2020-04-21 10:40:39 -0700

    trying to connect to db

--- app.js
+++ app.js
@@ -7,31 +7,58 @@ const app = express();
 const path = 'path';
 const PORT = process.env.PORT || 3306;
 
-//Database connection
-app.use(function (req, res, next) {
-  res.locals.connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'myqldbneoboard',
-    password: '20Ne0b0ard20',
-    database: 'Neoboard',
-  });
-  res.locals.connection.connect();
-  next();
+const pool = mysql.createPool({
+  host: 'myql-db-neoboard.ccauxyrnch3q.us-west-2.rds.amazonaws.com',
+  user: 'myqldbneoboard',
+  password: '20Ne0b0ard20',
+  port: 3306,
+  database: 'Neoboard',
 });
 
-app.get('/home', function (req, res) {
-  db.smoi.findAll({}).then(function (results) {
-    console.log(`Here are the results from the GET request: ${results}`);
-    res.json(results);
+pool.getConnection(function (err, conn) {
+  if (err) return res.send(400);
+
+  // if you got a connection...
+  conn.query('SELECT * FROM table WHERE id=? AND name=?', [id, name], function (
+    err,
+    rows
+  ) {
+    if (err) {
+      conn.release();
+      return res.send(400, 'Couldnt get a connection');
+    }
+
+    // for simplicity, just send the rows
+    res.send(rows);
+
+    // CLOSE THE CONNECTION
+    conn.release();
   });
 });
 
-app.listen(PORT, function () {
+/* connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+}); */
+
+/* app.listen(PORT, function () {
   console.log(
     `==> 🌎  Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`,
     PORT,
     PORT
   );
-});
+}); */
 
 /* "mysql -h mysql-db-neoboard.ccauxyrnch3q.us-west-2.rds.amazonaws.com -P 3306 -u myqldbneoboard -p" */
+
+//Database connection
+/* app.use(function (req, res, next) {
+    res.locals.connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'myqldbneoboard',
+      password: '20Ne0b0ard20',
+      database: 'Neoboard',
+    });
+    res.locals.connection.connect();
+    next();
+  }); */
